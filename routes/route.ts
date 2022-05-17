85b39780c30d35044130bf76f357dce85af43178
praburocking <prabumohan96@gmail.com>
2022-05-17 15:05:24 +0300

    clutters removed

--- routes/route.ts
+++ routes/route.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import {Ship,addOrUpdateShip} from '../models/models'
+import {addOrUpdateShip} from '../models/models'
 
 
 interface fromTemplate {
@@ -28,7 +28,6 @@ interface cargoTemplate {
 
 export const handleTo= async (request: Request, response: Response, next: NextFunction) => {
 
-console.log(request.body);
 let body:toTemplate=request.body;
 const savedData = await addOrUpdateShip({ id: body.extOrderId,to:body.toLocation});
 response.status(200).json(savedData);
@@ -36,8 +35,6 @@ response.status(200).json(savedData);
 }
 
 export const handleFrom= async  (request: Request, response: Response, next: NextFunction) => {
-
-console.log(request);
 let body:fromTemplate=request.body;
 const savedData = await addOrUpdateShip({ id: body.extOrderId,from:body.fromLocation});
 response.status(200).json(savedData);
@@ -45,7 +42,6 @@ response.status(200).json(savedData);
 
 
 export const handleCargo= async(request: Request, response: Response, next: NextFunction) => {
-  console.log(request);
   let body:cargoTemplate=request.body;
   const savedData = await addOrUpdateShip({ id: body.extOrderId,cargo_type:body.cargoType,cargo_amount:body.cargoAmount});
   response.status(200).json(savedData);
--- scheduler/pushDataToSeaber.ts
+++ scheduler/pushDataToSeaber.ts
@@ -5,21 +5,14 @@ import {pushToSeaber} from '../utils/httpUtils'
 export const pushDataToSeaberJob = async()=>{
 
 schedule.scheduleJob('*/1 * * * *',async function(){
-  console.log('The answer to life, the universe, and everything!');
-  let integData=await getDataForSeaberPush()
-  integData=integData.map((data:any)=>data.dataValues)
-//   for(var i = 0; i < integData.length; i++) {
-//     var obj = integData[i];
-
-//     console.log({...obj.dataValues});
-// }
-
-console.log(integData)
-let integStatus=await pushToSeaber(integData);
-if(integStatus){
-  let ids=integData.map((data:any)=>data.id)
-  await updateStatusByIds(ids);
-}
+    let integData=await getDataForSeaberPush()
+    integData=integData.map((data:any)=>data.dataValues)
+    console.log(integData)
+    let integStatus=await pushToSeaber(integData);
+    if(integStatus){
+      let ids=integData.map((data:any)=>data.id)
+      await updateStatusByIds(ids);
+    }
   
   
 })};
